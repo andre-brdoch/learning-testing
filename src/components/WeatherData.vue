8e6fd427f1bca74decbbfc3b49581824a6706793
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-05-03 15:50:30 +0200

    chore: broken

--- src/components/WeatherData.vue
+++ src/components/WeatherData.vue
@@ -82,7 +82,7 @@ const fetchWeather = async (): Promise<void> => {
     return {
       temperature: temperatures[i],
       cloudcover: cloudcovers[i],
-      date: new Date(dates[i]),
+      dates: new Date(dates[i]),
     };
   });
 };
