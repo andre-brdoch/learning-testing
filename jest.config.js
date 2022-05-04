87b34d6e4fe00f347214f0ce2748149d58bbcc3d
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-05-04 12:54:19 +0200

    chore: format lint runner

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   ...require('./test/jest.common'),
-  projects: ['./test/jest.client.js', './test/jest.lint.js'],
+  projects: [
+    './test/jest.client.js',
+    './test/jest.lint.js',
+    './test/jest.format.js',
+  ],
 };
---
+++ test/jest.format.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ...require('./jest.common'),
+  displayName: 'format',
+  preset: 'jest-runner-prettier',
+};
