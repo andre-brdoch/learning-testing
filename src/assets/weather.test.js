226779d60e0c112df7f1606adeb96a58af4737da
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-05-05 13:52:19 +0200

    feat: getCoordinatesByCityName()

--- src/assets/weather.test.js
+++ src/assets/weather.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const { getWeather } = require('./weather.ts');
+const { getCoordinatesByCityName, getWeather } = require('./weather.ts');
 
 jest.mock('axios');
 
@@ -8,31 +8,68 @@ afterEach(() => {
   axios.mockClear();
 });
 
+describe('getCoordinatesByCityName()', () => {
+  const stockholmCoordinates = {
+    lat: 59.3328,
+    long: 18.0645,
+  };
+  test('Returns { lat: 59.3328, long: 18.0645 } for cityName "Stockholm"', () => {
+    expect(getCoordinatesByCityName('Stockholm')).toEqual(stockholmCoordinates);
+  });
+  test('cityName is case insensitive', () => {
+    expect(getCoordinatesByCityName('stockholm')).toEqual(stockholmCoordinates);
+  });
+  test('Returns undefined for not supported cityName "Duckburg"', () => {
+    expect(getCoordinatesByCityName('Duckburg')).toBeUndefined();
+  });
+});
+
 describe('getWeather()', () => {
-  test('Returns the weather when provided valid coordinates', async () => {
-    const temperature = 24;
-    const cloudcover = 50;
-    const date = new Date();
-    const weather = {
+  const temperature = 24;
+  const cloudcover = 50;
+  const date = new Date();
+  const resolvedAxiosResponse = {
+    data: {
       current_weather: { temperature },
       hourly: {
         cloudcover: [cloudcover],
         temperature_2m: [temperature],
         time: [date],
       },
-    };
+    },
+  };
+  const resolvedWeatherResponse = {
+    current: { temperature },
+    hourly: [{ temperature, cloudcover, date }],
+  };
+
+  test('Returns the weather when provided valid coordinates', async () => {
     // mock axios resolve value
-    axios.mockResolvedValue({ data: weather });
+    axios.mockResolvedValue(resolvedAxiosResponse);
 
     const data = await getWeather({ lat: 0, long: 0 });
-    expect(data).toEqual({
-      current: { temperature },
-      hourly: [{ temperature, cloudcover, date }],
-    });
+    expect(data).toEqual(resolvedWeatherResponse);
   });
 
-  test('Throws an error when provided invalid coordinates without doing an HTTP request', async () => {
+  test('Returns the weather when provided supported city name', async () => {
+    // mock axios resolve value
+    axios.mockResolvedValue(resolvedAxiosResponse);
+
+    const data = await getWeather('Stockholm');
+    expect(data).toEqual(resolvedWeatherResponse);
+  });
+
+  test('Throws an error when provided invalid coordinates, without doing an HTTP request', async () => {
     await expect(getWeather({ lat: 666, long: 666 })).rejects.toThrow();
+    // validate that no axios call was made
+    expect(axios.mock.calls).toHaveLength(0);
+  });
+
+  test('Throws an "InvalidCityNameError" error when provided a non supported city name, without doing an HTTP request', async () => {
+    // can also use regex to test error message:
+    await expect(getWeather('Duckburg')).rejects.toThrow(
+      /InvalidCityNameError/
+    );
     // validate that no axios call was made
     expect(axios.mock.calls).toHaveLength(0);
   });
@@ -44,12 +81,4 @@ describe('getWeather()', () => {
       'AxiosError'
     );
   });
-
-  // test('Returns the weather when provided a valid city name', async () => {
-  //   //
-  // });
-
-  // test('Throws an error when provided an invalid city name', async () => {
-  //   //
-  // });
 });
--- src/assets/weather.ts
+++ src/assets/weather.ts
@@ -11,7 +11,46 @@ interface ApiPayload {
   };
 }
 
-export async function getWeather(coordinates: Coordinates): Promise<Weather> {
+export const cities: City[] = [
+  { lat: 55.70584, long: 13.19321, name: 'Lund' },
+  { lat: 59.3328, long: 18.0645, name: 'Stockholm' },
+  { lat: 46.0569, long: 14.5058, name: 'Ljubljana' },
+  { lat: 56.0465, long: 12.6945, name: 'Helsingborg' },
+  { lat: 60.4843, long: 15.434, name: 'Borlänge' },
+];
+
+export function getCoordinatesByCityName(
+  cityName: CityName
+): Coordinates | undefined {
+  const city = cities.find(
+    city => city.name.toLowerCase() === cityName?.toLowerCase()
+  );
+  if (!city) return undefined;
+  const { lat, long } = city;
+  return { lat, long };
+}
+
+// overload signatures
+export function getWeather(coordinates: Coordinates): Promise<Weather>;
+export function getWeather(cityName: CityName): Promise<Weather>;
+// implementation signature
+export async function getWeather(val: Coordinates | CityName) {
+  let coordinates: Coordinates;
+
+  if (typeof val === 'string') {
+    const possibleCoordinates = getCoordinatesByCityName(val);
+    if (!possibleCoordinates) {
+      throw new Error(
+        `InvalidCityNameError: "${val}" is not a supported city name! Please use one of: ${cities
+          .map(city => `"${city.name}"`)
+          .join(', ')}, or provide coordinates directly.`
+      );
+    }
+    coordinates = possibleCoordinates;
+  } else {
+    coordinates = val;
+  }
+
   const { lat, long } = coordinates;
   if (lat > 90 || long > 90 || lat < -90 || long < -90) {
     throw new Error('Invalid coordinates');
