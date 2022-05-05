265c0ac42bd4ad7d2372d7ade0edaf1b5fd01642
Andre Brdoch <andrebrdoch@yhahoo.de>
2022-05-05 11:20:44 +0200

    test: test getWeather(coordinates)

--- src/assets/weather.test.js
+++ src/assets/weather.test.js
@@ -1,7 +1,41 @@
-test('it works', () => {
-  expect(true).toBe(true);
-});
-test('it logs stuff', () => {
-  console.log('log log');
-  expect(null).toBeNull();
+import axios from 'axios';
+const { getWeather } = require('./weather.ts');
+
+jest.mock('axios');
+
+describe('getWeather', () => {
+  test('Returns the weather when provided valid coordinates', async () => {
+    const temperature = 24;
+    const cloudcover = 50;
+    const date = new Date();
+    const weather = {
+      current_weather: { temperature },
+      hourly: {
+        cloudcover: [cloudcover],
+        temperature_2m: [temperature],
+        time: [date],
+      },
+    };
+    // mock axios
+    axios.mockResolvedValue({ data: weather });
+
+    const data = await getWeather({ lat: 0, long: 0 });
+    expect(data).toEqual({
+      current: { temperature },
+      hourly: [{ temperature, cloudcover, date }],
+    });
+  });
+
+  test('Throws an error when provided invalid coordinates', async () => {
+    // mock failed axios request
+    axios.mockRejectedValue(new Error('AxiosError'));
+    return expect(getWeather({ lat: 666, long: 666 })).rejects.toThrow();
+  });
+
+  // test('Throws an error when provided a valid city name', async () => {
+  //   //
+  // });
+  // test('Throws an error when provided an invalid city name', async () => {
+  //   //
+  // });
 });
